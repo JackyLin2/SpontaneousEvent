177785c4b25e502eeaab8022d22f2b9ecb1e7ea0
JackyLin2 <jacky.jia.lin@gmail.com>
2021-08-31 15:21:30 -0400

    got current location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { CssBaseline, Grid} from '@material-ui/core'
 
+import { getPlacesData } from './api'
 import Header from './Components/Header/Header'
 import List from './Components/List/List'
 import Map from './Components/Map/Map'
 
 
 const App = () => {
+    
+    const [ places, setPlaces] = useState([])
+    const [coordinates , setCoordinates] = useState({})
+    const [bounds, setBounds] = useState(null)
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude}}) => {
+                setCoordinates({ lat: latitude, lng: longitude})
+        })
+    }, []) // get current location you're at 
+
+    useEffect(() => {
+        getPlacesData(bounds.sw, bounds.ne)
+            .then((data) => {
+                setPlaces(data)
+                console.log(data)
+            })
+    }, [coordinates, bounds]);
+
+
     return (
         <div>
             <CssBaseline/>
@@ -16,7 +37,13 @@ const App = () => {
                         <List/> 
                    </Grid> 
                    <Grid item xs={12} md={8}>  
-                        <Map/> 
+                        <Map  
+                            setCoordinates = { setCoordinates }  
+                            setBounds = { setBounds }
+                            coordinates = {coordinates}
+                            
+                        /> 
+                     
                    </Grid> 
             </Grid>
 
